acca15307a63e4756aa9dda47988ad4ab58482e6
ArielBenichou <videojr3000@users.noreply.github.com>
2024-12-03 17:03:45 +0200

    refactor(zig): 2024 02b - ahh not worky - need to calc premutation

--- zig/src/2024/02.zig
+++ zig/src/2024/02.zig
@@ -7,7 +7,7 @@ fn solution(path: []const u8, tolerate_bad_level: bool) !u64 {
     const reports = try readReports(allocator, path);
     var reports_safe_count: u64 = 0;
     for (reports.items) |report| {
-        reports_safe_count += @intFromBool(report.isSafe(tolerate_bad_level));
+        reports_safe_count += @intFromBool(report.isSafe(@intFromBool(tolerate_bad_level)));
     }
     return reports_safe_count;
 }
@@ -33,29 +33,47 @@ const Report = struct {
         self.allocator.free(self.levels);
     }
 
-    pub fn isSafe(self: Self, tolerate_bad_level: bool) bool {
+    pub fn isSafe(self: Self, bad_levels_tolerence: u64) bool {
         const levels = self.levels.items;
         if (levels.len <= 1) return true;
-        var bad_level_idx: ?usize = null;
-        var is_desc = levels[0] > levels[1];
+
+        var bad_levels_count: u64 = 0;
+        var negative_levels: u64 = 0;
+        var bad_negative_levels: u64 = 0;
+        // 1 2 7 8 9
         for (0..levels.len - 1) |i| {
-            const is_bad_level_index = bad_level_idx == i;
-            const offset: usize = @intFromBool(is_bad_level_index);
-            const a = if (is_desc) levels[i - offset] else levels[i + 1];
-            const b = if (is_desc) levels[i + 1] else levels[i - offset];
-            const is_bad_level = (a < b or (a - b < 1 or a - b > 3));
-            if (is_bad_level) {
-                if (tolerate_bad_level and bad_level_idx == null) {
-                    bad_level_idx = i + 1;
-                    if (bad_level_idx == 1 and levels.len > 2) {
-                        is_desc = levels[0] > levels[2];
-                    }
-                } else {
-                    return false;
+            const a: i64 = @intCast(levels[i]);
+            const b: i64 = @intCast(levels[i + 1]);
+            const diff = a - b;
+            if (@abs(diff) < 1 or @abs(diff) > 3) {
+                bad_levels_count += 1;
+                if (diff < 0) {
+                    bad_negative_levels += 1;
                 }
             }
+            if (diff < 0) {
+                negative_levels += 1;
+            }
         }
-        return true;
+
+        // NOTE: diffs is always one less thatn levels
+        const positive_levels: u64 = levels.len - 1 - negative_levels;
+        if (positive_levels > negative_levels) {
+            bad_levels_count += negative_levels - bad_negative_levels;
+        } else {
+            bad_levels_count += positive_levels - (bad_levels_count - bad_negative_levels);
+        }
+
+        std.debug.print("{any}\n", .{.{
+            .levels = levels,
+            .bad_levels_tolerence = bad_levels_tolerence,
+            .bad_levels_count = bad_levels_count,
+            .bad_negative_levels = bad_negative_levels,
+            .negative_levels = negative_levels,
+            .positive_levels = positive_levels,
+            .ok = bad_levels_count <= bad_levels_tolerence,
+        }});
+        return bad_levels_count <= bad_levels_tolerence;
     }
 };
 
@@ -89,20 +107,20 @@ fn readReports(allocator: std.mem.Allocator, path: []const u8) !std.ArrayList(Re
     return reports;
 }
 
-test "day 2 - part 1 - example" {
-    const path = "../inputs/2024/02_example.txt";
-    try std.testing.expectEqual(2, try solution(path, false));
-}
-
-test "day 2 - part 1" {
-    const path = "../inputs/2024/02_input.txt";
-    try std.testing.expectEqual(606, try solution(path, false));
-}
-//
-// test "day 2 - part 2 - example" {
+// test "day 2 - part 1 - example" {
 //     const path = "../inputs/2024/02_example.txt";
-//     try std.testing.expectEqual(4, try solution(path, true));
+//     try std.testing.expectEqual(2, try solution(path, false));
 // }
+//
+// test "day 2 - part 1" {
+//     const path = "../inputs/2024/02_input.txt";
+//     try std.testing.expectEqual(606, try solution(path, false));
+// }
+
+test "day 2 - part 2 - example" {
+    const path = "../inputs/2024/02_example.txt";
+    try std.testing.expectEqual(4, try solution(path, true));
+}
 
 // test "day 2 - part 2" {
 //     const path = "../inputs/2024/02_input.txt";
